7244fd0ad3eb8577e5ea22a3295ec92bbea91267
root <root@ip-172-31-44-81.ap-southeast-1.compute.internal>
2025-03-17 11:27:48 +0000

    mCI/CD

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertEquals("Testing JDK 17 Compatibility", "Testing JDK 17 Compatibility");
+    }
+}
